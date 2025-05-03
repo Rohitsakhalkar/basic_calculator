7acd690487fc244f9fae4d793e0858181ada6e6e
Rohitsakhalkar <rohitsakhalkar3@gmail.com>
2025-05-03 18:31:45 +0530

    first commit

---
+++ working_process.js
@@ -0,0 +1,69 @@
+let display = document.getElementById('display');
+ document.addEventListener('keydown', function(event) {{
+ switch (event.key) {
+ case '1':appendValue('1'); break;
+ case '2':appendValue('2'); break;
+  case '3':appendValue('3'); break;
+  case '4':appendValue('4'); break;
+  case '5':appendValue('5'); break;
+  case '6':appendValue('6'); break;
+  case '7':appendValue('7'); break;
+  case '8':appendValue('8'); break;
+  case '9':appendValue('9'); break;
+  case '0':appendValue('0'); break;
+  case '+':appendValue('+'); break;
+  case '-':appendValue('-'); break;
+  case '*':appendValue('*'); break;
+  case '/':appendValue('/'); break;
+  case 'Enter':
+    event.preventDefault(); 
+    calculateResult();
+    break;
+  case 'Backspace':
+    event.preventDefault(); 
+    deleteLast();
+    break;
+  case 'Escape':
+    event.preventDefault(); 
+    clearDisplay();
+    break;
+} 
+ }});1
+function appendValue(value) {
+  display.value += value;
+  scrollDisplay();
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  let lines = display.value.split('\n');
+  lines[lines.length - 1] = lines[lines.length - 1].slice(0, -1);
+  display.value = lines.join('\n');
+}
+
+function calculateResult() {
+    try {
+      let lines = display.value.split('\n');
+      let expression = lines[lines.length - 1];
+      let result = eval(expression);
+  
+      if (!isFinite(result)) {
+        display.value += `\n= Error\n`;
+      } else {
+        display.value += `\n= ${result}\n\n`; 
+      }
+  
+      scrollDisplay();
+    } catch (e) {
+      display.value += `\n= Error\n\n`;
+      scrollDisplay();
+    }
+  }
+  
+
+function scrollDisplay() {
+  display.scrollTop = display.scrollHeight;
+}
